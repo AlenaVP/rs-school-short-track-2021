857a4b1bfe1212953601ce0af0e30a2510d74b15
AlenaVP <poprugo@gmail.com>
2021-04-08 18:14:18 +0300

    feat: add a solution for '07-file-names'

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,24 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const output = names;
+  const map = {};
+  for (let i = 0; i < names.length; i++) {
+    map[names[i]] = !Object.prototype.hasOwnProperty.call(map, (names[i])) ? 0 : +map[names[i]] + 1;
+  }
+  const array = Object.entries(map);
+  for (let j = 0; j < array.length; j++) {
+    const name = array[j][0];
+    for (let i = output.length - 1; i >= 0; i--) {
+      if (output[i] === name && map[output[i]] > 0) {
+        output[i] = `${output[i]}(${map[output[i]]--})`;
+        map[output[i]] = !Object.prototype.hasOwnProperty.call(map, output[i])
+          ? 0 : +map[output[i]] + 1;
+      }
+    }
+  }
+  return output;
 }
 
 module.exports = renameFiles;
