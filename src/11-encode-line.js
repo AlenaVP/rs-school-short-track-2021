3355e7d90f69ce86688f6cff1c163ca86128e209
AlenaVP <poprugo@gmail.com>
2021-04-09 15:20:40 +0300

    feat: add a solution for '11-encode-line'

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (!str) {
+    return str;
+  }
+  const arr = str.split('');
+  let a = arr.shift();
+  let count = 1;
+  let encoded = '';
+  while (arr.length > 0) {
+    const next = arr.shift();
+    if (a === next) {
+      count++;
+    } else {
+      encoded = count === 1 ? encoded + a : encoded + count + a;
+      count = 1;
+    }
+    a = next;
+  }
+  encoded = count === 1 ? encoded + a : encoded + count + a;
+  return encoded;
 }
 
 module.exports = encodeLine;
