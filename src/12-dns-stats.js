38a3832091e5835914b7453fbf57230a704c25aa
AlenaVP <poprugo@gmail.com>
2021-04-10 10:29:55 +0300

    feat: add a solution for '12-dns-stats'

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,23 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const map = {};
+  for (let i = 0; i < domains.length; i++) {
+    let temp = [];
+    let part = domains[i];
+    let idx;
+    do {
+      temp.push(`.${part.split('.').reverse().join('.')}`);
+      idx = part.indexOf('.');
+      part = part.substring(idx + 1);
+    } while (idx !== -1);
+    temp = temp.reverse();
+    for (let j = 0; j < temp.length; j++) {
+      map[temp[j]] = !Object.prototype.hasOwnProperty.call(map, (temp[j])) ? 1 : +map[temp[j]] + 1;
+    }
+  }
+  return map;
 }
 
 module.exports = getDNSStats;
